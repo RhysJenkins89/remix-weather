df38f3fa1e823553fe971448e82ed7ce602be7a9
Rhys <rhys@rhysjenkins.uk>
2023-06-09 15:32:59 +0100

    Cleaned up index file

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -22,27 +22,15 @@ export const loader = async () => {
 
 // Update the db with the user's input
 export const action = async (formData) => {
-	// console.log({formData})
-
-	// const form = await request.formData();
-	// const city = form.get('city');
-
-	// We won't need form validation like this. We might need something else, however. 
-	// if (typeof city !== "string" || city === "") {
-	//     redirect('/new-quote')
-	//     throw new Error(`Form not submitted correctly.`);
-	// }
-	// const fields = { by, quote };
-
 	await db.city.create({ data: formData });
-	// return redirect('/');
+	// Do something else here
 }
 
 export default function Index() {
 	const { cities } = useLoaderData();
 	const submit = useSubmit()
 
-	// React data fetching
+	// Standard React data fetching
 	const [text, setText] = useState('')
 	const [userSearch, setUserSearch] = useState('')
 
@@ -52,50 +40,29 @@ export default function Index() {
 			const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${event.target.value}&count=10`)
 			const resData = await response.json()
 			setUserSearch(resData)
-			// reset(false)
 		} else {
 			setText('')
 			setUserSearch('')
 		}
 	}
 
+	// Call the submit function here
 	const handleClick = async (event) => {
-		console.log('item clicked')
-		console.log(event.currentTarget.getAttribute('data-city'))
-		// debugger
-		
-		// const form = await request.formData();
-		// const city = form.get('city');
-		const city = event.currentTarget.getAttribute('data-city') 
+		const city = event.currentTarget.getAttribute('data-city')
 		console.log('City form data:', city)
 		console.log(typeof city)
-		// let formData = new FormData();
-		// formData.append('name', 'new city');
-		// console.log('formData in handleClick:', formData)
 
-		// submit('city', {
-		// 	method: 'post'
-		// });
+		submit(null, {
+			action: "/logout",
+			method: "post",
+		});
 
-
-
-		// submit()
-
-		// submit(null, {
-		// 	action: "/logout",
-		// 	method: "post",
-		// });
-		  
-		// // same as
+		// same as
 		// <Form action="/logout" method="post" />;
 	}
 
 	return (
 		<div>
-			{/* <ResponsiveAppBar /> */}
-			{/* <Typography variant="h4" component="h1" gutterBottom>
-				Welcome to Remix!
-			</Typography> */}
 			<h1>Welcome to Remix!</h1>
 			<div>
 				<Link to='/login'>
@@ -107,14 +74,10 @@ export default function Index() {
 				userSearch.results ?
 					userSearch.results.map((item, index) => {
 						return (
-							// <div onClick={handleClick} key={index}>
-							// 	<p>{item.name}, {item.country}</p>
-							// </div>
-							// It's bad practice to use the array index as the key, but it'll do for now.
-
 							<form onClick={handleClick} key={index} data-city={item.name}>
-								<input name="city" value={item.name} readOnly/>
+								<p>{item.name}</p>
 							</form>
+							// It's bad practice to use the array index as the key, but it'll do for now.
 						)
 					})
 					:
